e36b06d18896edcea9d5463bb5c6884fb3ae45b7
ChandarAwesome <ajaygames00Gmail.com>
2023-03-20 21:41:22 +0530

    [Chandra]

---
+++ DemoAddition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NloggerDemo
+{
+    public class DemoAddition
+    {
+        Nlogger_Start nLog = new Nlogger_Start();  // create an object from the nloggger class
+        public  void Addition()
+        {
+           
+            int a = 10;
+            int b = 20;
+            //Console.WriteLine("Additon of the both numbers"+a + b);
+            int c=a+b;
+            nLog.LogDebug("Debuging : Addition Method");
+            nLog.LogInfo("result" + " "+c);
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using NloggerDemo.CRUD;
+
+namespace NloggerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Console.WriteLine("Hello, World!");
+        //    DemoAddition obj = new DemoAddition();
+        //    obj.Addition();
+
+          //  ArrayLists.CrudwithArrayList();
+            Dictionary.CrudwithDictionary();
+        }
+    }
+}
